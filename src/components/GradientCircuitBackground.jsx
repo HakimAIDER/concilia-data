a553f3568f4544ae3275b3c4de11358766d0b734
HakimAIDER <aiderhakim.1996@gmail.com>
2025-04-25 17:45:13 +0200

    Add files via upload

---
+++ src/components/GradientCircuitBackground.jsx
@@ -0,0 +1,15 @@
+const GradientCircuitBackground = () => {
+  return (
+    <div
+      style={{
+        backgroundImage: "url('/images/background-harmony.png')",
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        height: '100vh',
+        width: '100%',
+      }}
+    />
+  );
+};
+
+export default GradientCircuitBackground;
